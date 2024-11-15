ff1a7edcc972395f5c3b8ffadc58e407eba989cd
Daniel Ramos <danielramosacosta1@gmail.com>
2024-11-15 18:42:40 +0100

    move code to interface implementation

--- src/main/java/org/example/Statement.java
+++ src/main/java/org/example/Statement.java
@@ -2,19 +2,9 @@ package org.example;
 
 import java.util.List;
 
-public record Statement(String name1, List<Rental> rentals1, double totalAmount, int frequentRenterPoints) {
-
-  String asString() {
-    String result = "Rental Record for " + name1() + "\n";
-
-    for (Rental rental : rentals1()) {
-      result += "\t" + rental.getMovieTitle() + "\t" + rental.calculateAmount() + "\n";
-    }
-
-    // add footer lines
-    result += "Amount owed is " + totalAmount() + "\n";
-    result += "You earned " + frequentRenterPoints() + " frequent renter points";
-
-    return result;
-  }
-}
+public record Statement(
+    String name,
+    List<Rental> rentals,
+    double totalAmount,
+    int frequentRenterPoints
+) {}
--- src/main/java/org/example/StringStatementPrinter.java
+++ src/main/java/org/example/StringStatementPrinter.java
@@ -4,6 +4,16 @@ public class StringStatementPrinter implements StatementPrinter {
 
   @Override
   public String print(final Statement statement) {
-    return statement.asString();
+    String result = "Rental Record for " + statement.name() + "\n";
+
+    for (Rental rental : statement.rentals()) {
+      result += "\t" + rental.getMovieTitle() + "\t" + rental.calculateAmount() + "\n";
+    }
+
+    // add footer lines
+    result += "Amount owed is " + statement.totalAmount() + "\n";
+    result += "You earned " + statement.frequentRenterPoints() + " frequent renter points";
+
+    return result;
   }
 }

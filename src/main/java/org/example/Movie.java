d558d869b6876ff8aac1b30e6349e98456a60ab1
Daniel Ramos <danielramosacosta1@gmail.com>
2024-11-15 18:57:09 +0100

    refactor Movie removing getters and _

--- src/main/java/org/example/Movie.java
+++ src/main/java/org/example/Movie.java
@@ -6,31 +6,20 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
     public static final int REGULAR = 0;
 
-    private String _title;
-    private int _priceCode;
+    private final String title;
+    private final int priceCode;
 
     public Movie(String title, int priceCode) {
-        _title = title;
-        _priceCode = priceCode;
+        this.title = title;
+        this.priceCode = priceCode;
     }
 
-    public int getPriceCode() {
-        return _priceCode;
-    }
-
-    public void setPriceCode(int arg) {
-        _priceCode = arg;
-    }
     public String getTitle() {
-        return _title;
-    }
-
-    boolean isNewRelease() {
-        return getPriceCode() == NEW_RELEASE;
+        return title;
     }
 
     int calculateFrequentRenterPoints(final int daysRented1) {
-        if (isNewRelease() && daysRented1 > 1) {
+        if (priceCode == NEW_RELEASE && daysRented1 > 1) {
             return 2;
         } else {
             return 1;
@@ -41,7 +30,7 @@ public class Movie {
         double thisAmount = 0;
 
         //determine amounts for each line
-        switch (getPriceCode()) {
+        switch (priceCode) {
             case REGULAR:
                 thisAmount += 2;
                 if (daysRented1 > 2)
